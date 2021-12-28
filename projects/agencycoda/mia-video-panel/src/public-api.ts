4e859c81826eed6cc29b2ca6483f7323a4a8ec29
Matias Camiletti <matias.camiletti@gmail.com>
2021-12-27 22:00:39 -0300

    Fix version.

--- projects/agencycoda/mia-video-panel/src/public-api.ts
+++ projects/agencycoda/mia-video-panel/src/public-api.ts
@@ -2,4 +2,11 @@
  * Public API Surface of mia-video-panel
  */
 
+/**
+ * Pages
+ */
+ export * from './lib/pages/video-list/video-list.component';
+/**
+ * Modules
+ */
 export * from './lib/mia-video-panel.module';
